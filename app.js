a88e7b4e7dfe9c39c89c2f6c66f9407c9f0be207
andy-shi88 <shi88.andy@gmail.com>
2018-01-16 12:28:38 +0700

    add error handler, log, and migrate to es6

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const app = require('express')();
-const cors = require('cors');
-const bodyParser = require('body-parser');
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import { userController } from './controllers';
+// routes import
+import user from './routes/user';
+import item from './routes/item';
+import receiver from './routes/receiver';
+import mail from './routes/mail';
+
+const app = express();
 // const { SMTPServer } = require('smtp-server');
 require('dotenv').config();
 
 app.use(cors());
 
-// passport
-const User = require('./controllers/user');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'password' }, User.login));
+passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'password' }, userController.login.bind(userController)));
 
 // port setup
 app.set('port', process.env.PORT || 3000);
+// log setup
+app.use(logger('dev'));
 
 // bodyparser setup
 app.use(bodyParser.json());
@@ -34,16 +42,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //   console.log('Error %s', err.message);
 // });
 
-// routes
-const item = require('./routes/item');
-const user = require('./routes/user');
-const receiver = require('./routes/receiver');
-const mail = require('./routes/mail');
 
 app.use('/api/item', item);
 app.use('/api/user', user);
 app.use('/api/receiver', receiver);
 app.use('/api/mail', mail);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err.message);
+});
 
 app.listen(app.get('port'), () => {
   console.log(`app listening on ${app.get('port')}`);
