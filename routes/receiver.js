5cb11e763fe238824e4c8a09344415db8247aaba
andy-shi88 <shi88.andy@gmail.com>
2018-01-16 19:00:04 +0700

    use async await, finish 1 complete CRUD

--- routes/receiver.js
+++ routes/receiver.js
@@ -3,8 +3,24 @@ import { receiverController } from '../controllers';
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  receiverController.get(req, res);
+});
+
+router.get('/:id', (req, res) => {
+  receiverController.find(req, res);
+});
+
 router.post('/', (req, res) => {
   receiverController.create(req, res);
+});
+
+router.delete('/:id', (req, res) => {
+  receiverController.destroy(req, res);
+});
+
+router.put('/:id', (req, res) => {
+  receiverController.update(req, res);
 });
 
 module.exports = router;
