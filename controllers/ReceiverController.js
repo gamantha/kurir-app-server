c1205d98eb6cf5f35690213ce5b2eee0dd0bb0d4
andy-shi88 <shi88.andy@gmail.com>
2018-01-16 18:55:25 +0700

    use async await, finish 1 complete CRUD

--- controllers/ReceiverController.js
+++ controllers/ReceiverController.js
@@ -9,21 +9,63 @@ export default class ReceiverController {
     this.service = new ReceiverService();
   }
 
-  create(req, res) {
+  async create(req, res) {
     const {
       name, address, phone, city,
     } = req.body;
     const payload = {
       name, address, phone, city,
     };
-    this.service.create(payload, (result) => {
-      const response = new ResponseBuilder();
-      if (!result) {
-        response.setData({}).setMessage('fail to create receiver').setSuccess(false).build();
-      } else {
-        response.setData(result).setMessage('success').setSuccess(true).build();
-      }
-      res.status(201).json(response);
-    });
+    try {
+      const response = await this.service.create(payload);
+      res.status(201).json(new ResponseBuilder().setData(response).build());
+    } catch (error) {
+      res.status(400).json(new ResponseBuilder().setMessage(error).setSuccess(false).build());
+    }
+  }
+
+  async get(req, res) {
+    try {
+      const response = await this.service.findAll();
+      res.status(200).json(new ResponseBuilder().setData(response).build());
+    } catch (error) {
+      res.status(400).json(new ResponseBuilder().setMessage(error).setSuccess(false).build());
+    }
+  }
+
+  async find(req, res) {
+    const { id } = req.params;
+    try {
+      const response = await this.service.findOne({ id });
+      res.status(200).json(new ResponseBuilder().setData(response).build());
+    } catch (error) {
+      res.status(404).json(new ResponseBuilder().setMessage(error).setSuccess(false).build());
+    }
+  }
+
+  async destroy(req, res) {
+    const { id } = req.params;
+    try {
+      const response = await this.service.destroy({ id });
+      res.status(200).json(new ResponseBuilder().setData({}).build());
+    } catch (error) {
+      res.status(404).json(new ResponseBuilder().setMessage(error).setSuccess(false).build());
+    }
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const {
+      name, address, phone, city,
+    } = req.body;
+    const payload = {
+      name, address, phone, city,
+    };
+    try {
+      const response = await this.service.update(payload, { id });
+      res.status(200).json(new ResponseBuilder().setData(response).build());
+    } catch (error) {
+      res.status(404).json(new ResponseBuilder().setMessage(error).setSuccess(false).build());
+    }
   }
 }
