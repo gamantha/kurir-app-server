efaa122ad5ae293fc40384b5054896c1d23634c4
andy-shi88 <shi88.andy@gmail.com>
2018-01-17 16:50:18 +0700

    add auth middleware for verifying token

---
+++ helpers/Auth.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+import ResponseBuilder from './ResponseBuilder';
+
+/**
+ * Parse authorization header
+ * @param {String} token
+ */
+const parseToken = (token) => {
+  if (token.includes('bearer ')) {
+    return token.slice('bearer '.length);
+  }
+  throw Error('invalid token');
+};
+
+export default (req, res, next) => {
+  const { authorization } = req.headers;
+  const response = new ResponseBuilder();
+  if (typeof authorization === 'undefined' || authorization === '') {
+    // Auth token not provided
+    response
+      .setMessage('Authorization header not provided or empty.')
+      .setSuccess(false)
+      .build();
+    res.status(403).json(response);
+  }
+  // validate token
+  try {
+    const token = parseToken(authorization);
+    console.log(token, 'token parsed');
+    jwt.verify(token, process.env.SECRET);
+  } catch (error) {
+    response.setMessage(error.message).setSuccess(false).build();
+    res.status(401).json(response);
+  }
+  next();
+};
+
