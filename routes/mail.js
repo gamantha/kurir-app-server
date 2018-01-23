8440da06dbd0b94539c3501f963d61b430ca4511
Bhirawa Mbani <bhirmbani@gmail.com>
2018-01-23 14:20:36 +0700

    remove old mail controller

--- routes/mail.js
+++ routes/mail.js
@@ -3,10 +3,6 @@ import express from 'express';
 const router = express.Router();
 import { mailController } from '../controllers';
 
-// router.post('/sent-forgot-pass-code', mail.getVeriCodeForgotPassword);
-// router.post('/change-password', mail.changePassword);
-// router.post('/check-forgot-code', mail.checkForgotPassVeriCode);
-
 router.get('/check-email-is-valid/', (req, res) => {
   mailController.checkEmail(req, res);
 });
