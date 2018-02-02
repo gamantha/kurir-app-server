bb3d4fe0c16c5ec89a4b3fe9227e9317ac1b5602
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-02 16:28:40 +0700

    adding comment and change model def of admin table

--- models/admin.js
+++ models/admin.js
@@ -1,12 +1,12 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   const Admin = sequelize.define('Admin', {
     droppointId: DataTypes.INTEGER,
     name: DataTypes.STRING,
     username: DataTypes.STRING,
     password: DataTypes.STRING,
-    isDroppoint: DataTypes.BOOLEAN,
+    role: DataTypes.STRING, // @param {String} sysadmin, siteadmin
   });
-  Admin.associate = function (models) {
+  Admin.associate = function(models) {
     Admin.belongsTo(models.Droppoint, { foreignKey: 'droppointId' });
   };
   return Admin;
