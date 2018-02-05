45a9001bc1fb685d94134d72a8613fe95b8e33c9
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-05 15:06:30 +0700

    cleaning up

--- models/category.js
+++ models/category.js
@@ -1,10 +1,7 @@
-
-
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   const Category = sequelize.define('Category', {
     name: DataTypes.STRING,
   });
-  Category.associate = function (models) {
-  }
+  Category.associate = function(models) {};
   return Category;
 };
--- models/reputation.js
+++ models/reputation.js
@@ -1,15 +1,13 @@
-
-
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   const Reputation = sequelize.define('Reputation', {
     courierId: DataTypes.INTEGER,
     senderId: DataTypes.INTEGER,
     value: DataTypes.INTEGER,
     comment: DataTypes.STRING,
   });
-  Reputation.associate = function (models) {
-    Reputation.belongsTo(models.Courier, { foreignKey: 'courierId '});
-    Reputation.belongsTo(models.Sender, { foreignKey: 'senderId '});
-  }
+  Reputation.associate = function(models) {
+    Reputation.belongsTo(models.Courier, { foreignKey: 'courierId ' });
+    Reputation.belongsTo(models.Sender, { foreignKey: 'senderId ' });
+  };
   return Reputation;
 };
