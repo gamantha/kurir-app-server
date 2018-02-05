69cdadb055d5bdde8866cd8fab3fdcda3deb28b7
andy-shi88 <shi88.andy@gmail.com>
2018-02-05 21:09:20 +0700

    #35 relogin after logout test

--- test/auth/login_test.js
+++ test/auth/login_test.js
@@ -114,6 +114,7 @@ describe('Login', () => {
           res.body.should.include.keys(BASE_RESPONSE_STRUCTURE);
           res.body.data.should.include.keys(TOKEN_RESPONSE_STRUCTURE);
           res.body.meta.success.should.be.eql(true);
+          accessToken = res.body.data.accessToken;
           done();
         });
     });
@@ -133,7 +134,7 @@ describe('Login', () => {
         .post('/api/user/logout')
         .set('Authorization', `bearer ${accessToken}`)
         .end((err, res) => {
-          res.should.have.status(404);
+          res.should.have.status(401);
           res.body.should.include.keys(BASE_RESPONSE_STRUCTURE);
           res.body.meta.success.should.be.eql(false);
           done();
@@ -142,6 +143,23 @@ describe('Login', () => {
   });
 
   describe('deactivate account', () => {
+    it('should return re-logged in successfully', (done) => {
+      chai.request(app)
+        .post('/api/user/login')
+        .send({
+          username: user.email,
+          password: user.password,
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.include.keys(BASE_RESPONSE_STRUCTURE);
+          res.body.data.should.include.keys(TOKEN_RESPONSE_STRUCTURE);
+          accessToken = res.body.data.accessToken;
+          refreshToken = res.body.data.refreshToken;
+          res.body.meta.success.should.be.eql(true);
+          done();
+        });
+    });
     it('should return 200', (done) => {
       chai.request(app)
         .delete('/api/user/deactivate')
