b604403c1f85a9f3fd3154b1688e3152e52abc5c
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-08 20:02:34 +0700

    update bodyparser setup to accomodate base64 upload img

--- app.js
+++ app.js
@@ -32,8 +32,14 @@ app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 
 // bodyparser setup
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 app.use('/api/item', item);
 app.use('/api/user', user);
