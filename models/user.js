58bab7a991a1d9522b3fab954b50c837c6096100
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-08 16:30:47 +0700

    remove foreignKey config in user and courier proposal association so include can work

--- models/user.js
+++ models/user.js
@@ -51,7 +51,8 @@ module.exports = function(sequelize, DataTypes) {
     User.hasMany(models.Token, { foreignKey: 'userId' });
     User.hasOne(models.Courier, { foreignKey: 'userId' });
     User.hasOne(models.Droppoint, { foreignKey: 'userId' });
-    User.hasOne(models.CourierProposal, { foreignKey: 'userId' });
+    // for hasOne and belongsTo, should not have foreignKey config
+    User.hasOne(models.CourierProposal);
   };
   User.prototype.toJSON = function() {
     let values = Object.assign({}, this.get());
