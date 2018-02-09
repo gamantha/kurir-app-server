b414264e932b47cb157cfd0511066de8a6cbc1d2
andy-shi88 <shi88.andy@gmail.com>
2018-02-09 20:48:30 +0700

    #KA-87 add courier proposal service

--- services/index.js
+++ services/index.js
@@ -6,6 +6,7 @@ import TokenService from './TokenService';
 import GoogleService from './GoogleService';
 import FacebookService from './FacebookService';
 import DroppointService from './DroppointService';
+import CourierProposalService from './CourierProposalService';
 
 /**
  * Services export
@@ -19,4 +20,5 @@ export {
   FacebookService,
   MailService,
   DroppointService,
+  CourierProposalService,
 };
