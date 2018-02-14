a7042f7ad5c4407c160b3469b992021484feb5e0
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-14 14:32:37 +0700

    move courier proposal service

--- services/CourierProposalService.js
+++ services/CourierProposalService.js
@@ -7,5 +7,93 @@ export default class CourierProposalService extends BaseService {
    */
   constructor() {
     super(models.CourierProposal);
+    this.userModel = models.User;
+  }
+
+  async proposalRejected(status, rejectReason, userId) {
+    const updated = await super.update(
+      {
+        status,
+        rejectDate: new Date(),
+        acceptDate: null,
+        rejectReason,
+      },
+      {
+        UserId: userId,
+      },
+      {
+        returning: true,
+        plain: true,
+      }
+    );
+    await this.userModel.update(
+      {
+        role: 'sender',
+      },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+    return updated;
+  }
+
+  async proposalAccepted(status, rejectReason, userId) {
+    const updated = await super.update(
+      {
+        status,
+        acceptDate: new Date(),
+        rejectDate: null,
+        rejectReason: null,
+      },
+      {
+        UserId: userId,
+      },
+      {
+        returning: true,
+        plain: true,
+      }
+    );
+    await this.userModel.update(
+      {
+        role: 'sender+kurir',
+      },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+    return updated;
+  }
+
+  async proposalWaiting(status, rejectReason, userId) {
+    const updated = await super.update(
+      {
+        status,
+        rejectDate: null,
+        acceptDate: null,
+        rejectReason: null,
+      },
+      {
+        UserId: userId,
+      },
+      {
+        returning: true,
+        plain: true,
+      }
+    );
+    await this.update(
+      {
+        role: 'sender',
+      },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+    return updated;
   }
 }
