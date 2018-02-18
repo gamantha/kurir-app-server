612cf3f1b36489b038d51f531d879f9b199f705a
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-18 10:29:41 +0700

    delete /dist

--- dist/helpers/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var buildEmailValidationUri = exports.buildEmailValidationUri = function buildEmailValidationUri(email) {
-  return "https://api.mailgun.net/v3/address/validate?address=" + email + "&api_key=" + process.env.mailgunPublicValidationKey;
-};
