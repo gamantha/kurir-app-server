c42bbd03c47fe31c79e40d038c3d54d10ea09ef9
andy-shi88 <shi88.andy@gmail.com>
2018-03-01 19:55:19 +0700

    update item model

--- models/item.js
+++ models/item.js
@@ -1,9 +1,9 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   const Item = sequelize.define('Item', {
     ticketNumber: DataTypes.STRING,
     courierId: DataTypes.INTEGER,
     senderId: DataTypes.INTEGER,
-    categoryId: DataTypes.INTEGER,
+    category: DataTypes.STRING,
     ReceiverId: DataTypes.INTEGER,
     from: DataTypes.STRING,
     to: DataTypes.STRING,
@@ -16,53 +16,13 @@ module.exports = function(sequelize, DataTypes) {
     cost: DataTypes.STRING,
     reward: DataTypes.STRING,
     note: DataTypes.STRING,
-    statusMsg: DataTypes.STRING,
-    isCustomPickupAddress: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isExpensive: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isOnFirstDropsite: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isPicked: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isOnStartDroppoint: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isOnTravel: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isOnEndDroppoint: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isOnTheWayToReceiver: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isReceived: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isCanceled: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
+    status: DataTypes.STRING,
+    deadline: DataTypes.DATE,
   });
-  Item.associate = function(models) {
+  Item.associate = function (models) {
     Item.belongsTo(models.Sender, { foreignKey: 'senderId' });
     Item.belongsTo(models.Receiver);
     Item.belongsTo(models.Courier, { foreignKey: 'courierId' });
-    Item.belongsTo(models.Category, { foreignKey: 'categoryId' });
     Item.hasMany(models.Droppoint, { foreignKey: 'itemId' });
     Item.hasOne(models.Pickup, { foreignKey: 'itemId' });
   };
