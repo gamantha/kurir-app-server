660a2add667c93e7c7a8f31f933d7b213f415540
andy-shi88 <shi88.andy@gmail.com>
2018-03-01 19:55:29 +0700

    update item routes

--- routes/item.js
+++ routes/item.js
@@ -1,9 +1,30 @@
-const express = require('express');
+import express from 'express';
+import { itemController } from '../controllers';
+import Auth from '../helpers/Auth';
+
 
 const router = express.Router();
-const item = require('../controllers/item');
 
-router.post('/create', item.create);
-router.put('/custom-address/:itemId/', item.itemHasCustomPickupAddress);
+router.use(Auth);
+
+router.get('/', (req, res) => {
+  itemController.get(req, res);
+});
+
+router.get('/:id', (req, res) => {
+  itemController.find(req, res);
+});
+
+router.post('/', (req, res) => {
+  itemController.create(req, res);
+});
+
+router.delete('/:id', (req, res) => {
+  itemController.destroy(req, res);
+});
+
+router.put('/:id', (req, res) => {
+  itemController.update(req, res);
+});
 
 module.exports = router;
