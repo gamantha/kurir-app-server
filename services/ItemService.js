c78d73000338a579f552c1937a0326dcc072b37a
andy-shi88 <shi88.andy@gmail.com>
2018-03-01 19:56:36 +0700

    add item service

---
+++ services/ItemService.js
@@ -0,0 +1,11 @@
+import BaseService from './BaseService';
+import models from '../models';
+
+export default class ItemService extends BaseService {
+  /**
+   * Item specific service class
+   */
+  constructor() {
+    super(models.Item);
+  }
+}
