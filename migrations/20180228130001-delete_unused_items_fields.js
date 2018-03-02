7115e245e33f1236e863fd8be6b5db43089e85d5
andy-shi88 <shi88.andy@gmail.com>
2018-03-02 10:20:37 +0700

    remove delete is migration, done in another migrations

--- migrations/20180228130001-delete_unused_items_fields.js
+++ migrations/20180228130001-delete_unused_items_fields.js
@@ -3,60 +3,24 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return [
-      queryInterface.removeColumn('Items', 'isExpensive'),
-      queryInterface.removeColumn('Items', 'isPicked'),
-      queryInterface.removeColumn('Items', 'isOnStartDroppoint'),
-      queryInterface.removeColumn('Items', 'isOnFirstDropsite'),
-      queryInterface.removeColumn('Items', 'isOnTravel'),
-      queryInterface.removeColumn('Items', 'isOnEndDroppoint'),
-      queryInterface.removeColumn('Items', 'isOnTheWayToReceiver'),
-      queryInterface.removeColumn('Items', 'isReceived'),
-      queryInterface.removeColumn('Items', 'isCanceled'),
       queryInterface.removeColumn('Items', 'statusMsg'),
       queryInterface.removeColumn('Items', 'categoryId'),
       queryInterface.dropTable('Categories'),
       queryInterface.removeColumn('Items', 'CourierId'),
-      queryInterface.addColumn('Items', 'courierId', {
-        type: Sequelize.INTEGER,
-      }),
       queryInterface.addColumn('Items', 'status', {
         type: Sequelize.STRING,
       }),
       queryInterface.addColumn('Items', 'category', {
         type: Sequelize.STRING,
       }),
+      queryInterface.addColumn('Items', 'courierId', {
+        type: Sequelize.INTEGER,
+      }),
     ];
   },
 
   down: (queryInterface, Sequelize) => {
     return [
-      queryInterface.addColumn('Items', 'isExpensive', {
-        type: Sequelize.BOOLEAN
-      }),
-      queryInterface.addColumn('Items', 'isPicked', {
-        type: Sequelize.BOOLEAN
-      }),
-      queryInterface.addColumn('Items', 'isOnStartDroppoint', {
-        type: Sequelize.BOOLEAN
-      }),
-      queryInterface.addColumn('Items', 'isOnFirstDropsite', {
-        type: Sequelize.BOOLEAN
-      }),
-      queryInterface.addColumn('Items', 'isOnTravel', {
-        type: Sequelize.BOOLEAN
-      }),
-      queryInterface.addColumn('Items', 'isOnEndDroppoint', {
-        type: Sequelize.BOOLEAN
-      }),
-      queryInterface.addColumn('Items', 'isOnTheWayToReceiver', {
-        type: Sequelize.BOOLEAN
-      }),
-      queryInterface.addColumn('Items', 'isReceived', {
-        type: Sequelize.BOOLEAN
-      }),
-      queryInterface.addColumn('Items', 'isCanceled', {
-        type: Sequelize.BOOLEAN
-      }),
       queryInterface.addColumn('Items', 'statusMsg', {
         type: Sequelize.STRING,
       }),
@@ -67,6 +31,7 @@ module.exports = {
       queryInterface.removeColumn('Items', 'category'),
 
       queryInterface.removeColumn('Items', 'courierId'),
+
       queryInterface.addColumn('Items', 'CourierId', {
         type: Sequelize.INTEGER,
       }),
