b0ce7d41c3afe25f71346b1e2ab0dd684d2daa2c
Bhirawa Mbani <bhirmbani@gmail.com>
2018-03-05 10:33:36 +0700

    item route

--- routes/item.js
+++ routes/item.js
@@ -2,14 +2,8 @@ import express from 'express';
 import { itemController } from '../controllers';
 import Auth from '../helpers/Auth';
 
-
 const router = express.Router();
 
-<<<<<<< HEAD
-router.post('/create', Auth, item.create);
-router.get('/', item.get);
-// router.put('/custom-address/:itemId/', item.itemHasCustomPickupAddress);
-=======
 router.use(Auth);
 
 router.get('/', (req, res) => {
@@ -31,6 +25,5 @@ router.delete('/:id', (req, res) => {
 router.put('/:id', (req, res) => {
   itemController.update(req, res);
 });
->>>>>>> aa74160a52fc4cdb2acc9bea48b3d9d28815fb1a
 
 module.exports = router;
