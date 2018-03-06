c55c56e9f821bf1d25b72d85573f857060fafa8d
Bhirawa Mbani <bhirmbani@gmail.com>
2018-03-06 17:57:36 +0700

    updating models/index
    for test in postgres

--- models/index.js
+++ models/index.js
@@ -1,12 +1,13 @@
 'use strict';
 
-import rawConfig from '../config/config.json';
+// import rawConfig from '../config/config.json';
 var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || 'development';
-var config = rawConfig[env];
+var config = require(__dirname + '/../config/config.json')[env];
+// var config = rawConfig[env];
 var db = {};
 
 if (config.use_env_variable) {
@@ -22,17 +23,17 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter(function (file) {
+  .filter(function(file) {
     return (
       file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
     );
   })
-  .forEach(function (file) {
+  .forEach(function(file) {
     var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function (modelName) {
+Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
