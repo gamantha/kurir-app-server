7f22eb9b1fd73ff189d5eec0935e5c005fc78f81
andy-shi88 <shi88.andy@gmail.com>
2018-03-22 19:07:23 +0700

    #67 fix lint

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -1,4 +1,4 @@
-import moment from 'moment'
+import moment from 'moment';
 import { ItemService, ReceiverService } from '../services/index';
 import ResponseBuilder from '../helpers/ResponseBuilder';
 
