e36400396b80e75d51b67c440dc54a1f89aac983
Bhirawa Mbani <bhirmbani@gmail.com>
2018-03-27 16:03:00 +0700

    delete dist and update baseUrl

--- dist/models/category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function (sequelize, DataTypes) {
-  var Category = sequelize.define('Category', {
-    name: DataTypes.STRING
-  });
-  Category.associate = function (models) {};
-  return Category;
-};
